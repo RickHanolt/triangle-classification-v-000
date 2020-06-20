4b2e1bfb34aba283fab2f37bd367d369e8711185
Richard Hanolt <rhanolt@gmail.com>
2020-06-20 22:15:06 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,40 +1,7 @@
-require 'pry'
-
 class Triangle
-  attr_accessor :side_a, :side_b, :side_c
+  attr_accessor :a, :b, :c
 
-  def initialize(side_a, side_b, side_c)
-    @side_a = side_a
-    @side_b = side_b
-    @side_c = side_c
-  end
-
-  def kind
-    if ((@side_a + @side_b) <= @side_c || (@side_a + @side_c) <= @side_b || (@side_c + @side_b) <= @side_a) || @side_a <= 0 || @side_b <= 0 || @side_c <= 0
-      begin
-        raise TriangleError
-      rescue TriangleError => error
-        puts error.message
-      end
-    end
-
-    if @side_a == @side_b && @side_a == @side_c
-      return :equilateral
-    end
-
-    if @side_a == @side_b || @side_a == @side_c || @side_b == @side_c
-      return :isosceles
-    end
-
-    if @side_a != @side_b && @side_a != @side_c && @side_b != @side_c
-      return :scalene
-    end
-  end
-
-  class TriangleError < StandardError
-    def message
-      "This is not a valid triangle."
-    end
-  end
-
+  def initialize(a, b, c)
+    @a = a
+    @b = b
 end
