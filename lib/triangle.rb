d49b491b36477684ab2b3a7bd24c640a9dedef79
Richard Hanolt <rhanolt@gmail.com>
2020-06-20 22:26:30 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -18,7 +18,7 @@ class Triangle
   end
 
   def valid_triangle?
-    if (@a <= 0 || @b <= 0 || @c <= 0) || ((@a + @b) <= @c || (@a + @c) <= @b || (@b + @c) <= @a)
+    if !(@a <= 0 || @b <= 0 || @c <= 0) || ((@a + @b) <= @c || (@a + @c) <= @b || (@b + @c) <= @a)
       raise TriangleError
     end
   end
